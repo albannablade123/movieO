bf5e44204ed7f9a2d00325e3a4d5840b0576a31f
pwnflakes <pwnflakes25@gmail.com>
2020-11-22 19:28:52 +0300

    finishing movie_detail page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
+        accentColor: Colors.deepOrangeAccent,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       darkTheme: ThemeData.dark(),
